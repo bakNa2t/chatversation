cccf0b4a75b415267907943e90b1c7454da9c7b4
bakNa2t <ok2.lock@gmail.com>
2024-10-22 15:30:27 +0300

    Added nextui-org to tailwind cfg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { Button } from "@nextui-org/react";
+
 const App = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex flex-col gap-4 justify-center items-center">
       <h1 className="flex justify-center items-center text-6xl">
         Wellcome to{" "}
         <em className="ml-2">
           <strong> Chatversation</strong>
         </em>
       </h1>
+
+      <Button size="lg" color="primary">
+        Get Started
+      </Button>
     </div>
   );
 };
