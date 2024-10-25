6e3f23b28c7fd48d5665c6f4fdf3cbe9650f37f3
bakNa2t <ok2.lock@gmail.com>
2024-10-25 09:20:20 +0300

    Added appwrite config

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,13 @@
+import { Account, Client } from "appwrite";
+
+export const appwriteConfig = {
+  url: import.meta.env.VITE_APPWRITE_URL,
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+
+client.setEndpoint(appwriteConfig.url);
+client.setProject(appwriteConfig.projectId);
+
+export const account = new Account(client);
