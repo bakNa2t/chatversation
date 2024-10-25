8b7aeee0714c9422c015f6d6698224e99e93eff7
bakNa2t <ok2.lock@gmail.com>
2024-10-25 16:57:02 +0300

    Added updateUser and resetState actions

--- src/lib/zustand/useStore.ts
+++ src/lib/zustand/useStore.ts
@@ -4,10 +4,13 @@ import { devtools, persist } from "zustand/middleware";
 
 type States = {
   userSession: Models.Session | object;
+  user: Models.User<Models.Preferences> | object;
 };
 
 type Actions = {
   updateUserSession: (session: Models.Session) => void;
+  updateUser: (user: Models.User<Models.Preferences>) => void;
+  resetState: () => void;
 };
 
 export const useStore = create<States & Actions>()(
@@ -15,9 +18,19 @@ export const useStore = create<States & Actions>()(
     persist(
       (set) => ({
         userSession: {},
+        user: {},
         updateUserSession: (session: Models.Session) =>
           set(() => ({
             userSession: session,
+          })),
+        updateUser: (user: Models.User<Models.Preferences>) =>
+          set(() => ({
+            user: user,
+          })),
+        resetState: () =>
+          set(() => ({
+            user: {},
+            userSession: {},
           })),
       }),
       { name: "chatversation_user_store" }
