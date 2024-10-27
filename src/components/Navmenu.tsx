0c38ee18287227859d6e7aa0173117033e6b770f
bakNa2t <ok2.lock@gmail.com>
2024-10-27 17:15:55 +0300

    Updated nav btn for logout style

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -18,30 +18,13 @@ const Navmenu = () => {
         />
         <p className="font-bold text-inherit">Chatversation</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
+        <NavbarItem>User</NavbarItem>
+
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
+          <Button as={Link} color="danger" href="#" variant="flat">
+            Logout
           </Button>
         </NavbarItem>
       </NavbarContent>
