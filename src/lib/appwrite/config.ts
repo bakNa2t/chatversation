78a10fa7869f39ca2ecf0da0a705f9afd4f0c136
bakNa2t <ok2.lock@gmail.com>
2024-10-28 14:11:58 +0300

    Added databases id and community collection id

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,8 +1,11 @@
-import { Account, Client } from "appwrite";
+import { Account, Client, Databases } from "appwrite";
 
 export const appwriteConfig = {
   url: import.meta.env.VITE_APPWRITE_URL,
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  communitiesCollectionId: import.meta.env
+    .VITE_APPWRITE_COMMUNITY_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -11,3 +14,4 @@ client.setEndpoint(appwriteConfig.url);
 client.setProject(appwriteConfig.projectId);
 
 export const account = new Account(client);
+export const databases = new Databases(client);
