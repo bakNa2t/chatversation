54a260452db7ba8c3f8ef7b2583c5bb6b91ea071
bakNa2t <ok2.lock@gmail.com>
2024-10-29 15:12:21 +0300

    Added chatStore

---
+++ src/lib/zustand/chatStore.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { Models } from "appwrite";
+import { devtools } from "zustand/middleware";
+
+type States = {
+  chats: Array<Models.Document> | [];
+};
+
+type Actions = {
+  addChat: (data: Models.Document) => void;
+  addChats: (data: Array<Models.Document>) => void;
+};
+
+export const chatStore = create<States & Actions>()(
+  devtools((set) => ({
+    chats: [],
+
+    addChat: (data: Models.Document) =>
+      set((state) => ({
+        chats: [data, ...state.chats],
+      })),
+
+    addChats: (data: Array<Models.Document>) =>
+      set(() => ({
+        chats: data,
+      })),
+  }))
+);
