d5369189648f8f2102b263d0cb60b07ddad6766c
bakNa2t <ok2.lock@gmail.com>
2024-10-29 11:30:48 +0300

    Added certain community to nextui Card

--- src/components/CommunitiesList.tsx
+++ src/components/CommunitiesList.tsx
@@ -2,11 +2,15 @@ import { useEffect, useRef, useState } from "react";
 import { appwriteConfig, databases } from "../lib/appwrite/config";
 import { AppwriteException, Query } from "appwrite";
 import { toast } from "react-toastify";
-import { Spinner } from "@nextui-org/react";
+import { Card, CardBody, Spinner } from "@nextui-org/react";
+
+import { communityStore } from "../lib/zustand/communityStore";
 
 const CommunitiesList = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const isFetched = useRef(false);
+
+  const communityState = communityStore();
 
   useEffect(() => {
     if (!isFetched.current) {
@@ -21,6 +25,7 @@ const CommunitiesList = () => {
         .then((res) => {
           console.log("The response was: ", res);
           setIsLoading(false);
+          communityState.addCommunities(res.documents);
         })
         .catch((error: AppwriteException) => {
           setIsLoading(false);
@@ -31,9 +36,25 @@ const CommunitiesList = () => {
     }
   }, []);
 
-  if (isLoading) <Spinner />;
+  if (isLoading)
+    return (
+      <div className="flex justify-center">
+        <Spinner color="secondary" />
+      </div>
+    );
 
-  return <div>Communities List</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-4">
+      {communityState.communities.length > 0 &&
+        communityState.communities.map((community) => (
+          <Card key={community.$id}>
+            <CardBody>
+              <h1 className="text-xl font-bold">{community["name"]}</h1>
+            </CardBody>
+          </Card>
+        ))}
+    </div>
+  );
 };
 
 export default CommunitiesList;
