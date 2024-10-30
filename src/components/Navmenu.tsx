873553da80ef888d90bc9f3ecaf1dcadc5f7c439
bakNa2t <ok2.lock@gmail.com>
2024-10-30 11:33:58 +0300

    Added logo router Link to Home

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -7,6 +7,7 @@ import {
 import { Models } from "appwrite";
 import { userStore } from "../lib/zustand/userStore";
 import ModalLogout from "./ModalLogout";
+import { Link } from "react-router-dom";
 
 const Navmenu = () => {
   const user = userStore(
@@ -14,14 +15,16 @@ const Navmenu = () => {
   );
 
   return (
-    <Navbar>
-      <NavbarBrand>
-        <img
-          src="/assets/images/chat-logo.png"
-          alt="Logo"
-          className="w-12 h-12 sm:w-14 sm:h-14 rounded-full"
-        />
-        <p className="font-bold text-inherit">Chatversation</p>
+    <Navbar className="bg-fuchsia-300/50">
+      <NavbarBrand className="gap-2">
+        <Link to="/">
+          <img
+            src="/assets/images/chat-logo.png"
+            alt="Logo"
+            className="w-12 h-12 sm:w-14 sm:h-14 rounded-full"
+          />
+        </Link>
+        <p className="font-bold text-xl sm:text-2xl">Chatversation</p>
       </NavbarBrand>
 
       <NavbarContent justify="end">
