02354104701d6685946b6a8f174a9020971eb543
bakNa2t <ok2.lock@gmail.com>
2024-10-30 09:49:51 +0300

    Added addChat action state to send message to db

--- src/_root/pages/Chatbox.tsx
+++ src/_root/pages/Chatbox.tsx
@@ -1,12 +1,44 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppwriteException, ID, Models } from "appwrite";
+import { toast } from "react-toastify";
 
 import { Input } from "@nextui-org/react";
 
+import { userStore } from "../../lib/zustand/userStore";
+import { appwriteConfig, databases } from "../../lib/appwrite/config";
+import { chatStore } from "../../lib/zustand/chatStore";
+
 const Chatbox = () => {
   const [message, setMessage] = useState("");
+  const { id } = useParams();
+  const user = userStore(
+    (state) => state.user
+  ) as Models.User<Models.Preferences>;
+
+  const chatState = chatStore();
 
   const handleMessageSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    databases
+      .createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.chatboxesCollectionId,
+        ID.unique(),
+        {
+          message: message,
+          user_id: user.$id,
+          community_id: id,
+          name: user.name,
+        }
+      )
+      .then((res) => {
+        chatState.addChat(res);
+      })
+      .catch((error: AppwriteException) => {
+        toast(error.message, { theme: "colored" });
+      });
   };
 
   return (
@@ -38,6 +70,7 @@ const Chatbox = () => {
         )}
       </div>
 
+      {/* Input message block */}
       <div className="fixed p-4 bottom-0 left-0 right-0 bg-fuchsia-200">
         <form onSubmit={handleMessageSubmit}>
           <div className="flex items-center space-x-2">
