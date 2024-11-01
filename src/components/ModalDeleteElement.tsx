66d2ad041c89df623b11aa796edb88789d342a5b
bakNa2t <ok2.lock@gmail.com>
2024-11-01 16:07:49 +0300

    Updated comp props to pass icon styles and elem name

---
+++ src/components/ModalDeleteElement.tsx
@@ -0,0 +1,63 @@
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Button,
+} from "@nextui-org/react";
+
+const ModalDeleteMessage = ({
+  handleDeleteElement,
+  nameElement,
+  btnStyles,
+}: {
+  handleDeleteElement: () => void;
+  nameElement?: string;
+  btnStyles?: string;
+}) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button isIconOnly variant="light" radius="full" className={btnStyles}>
+        <img
+          src="/assets/icons/trash.svg"
+          alt="trash"
+          className="cursor-pointer"
+          onClick={onOpen}
+        />
+      </Button>
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-2xl font-bold">
+                Delete {nameElement}
+              </ModalHeader>
+              <ModalBody>
+                <h1 className="text-xl font-semibold">Are you sure?</h1>
+                <p>
+                  This {nameElement} will be permanently deleted. Please
+                  confirm.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={onClose}>
+                  Cancel
+                </Button>
+                <Button color="danger" onPress={handleDeleteElement}>
+                  Confirm
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalDeleteMessage;
