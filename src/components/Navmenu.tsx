1aa6b01bd43bc88905d2f6dab584ac8eedc81ee6
bakNa2t <ok2.lock@gmail.com>
2024-11-04 10:18:26 +0300

    Added isMobile to show/hide app name

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import {
   Navbar,
   NavbarBrand,
@@ -10,9 +12,19 @@ import ModalLogout from "./ModalLogout";
 import { Link } from "react-router-dom";
 
 const Navmenu = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
   const user = userStore(
     (state) => state.user as Models.User<Models.Preferences>
   );
+
+  useEffect(() => {
+    if (screen.width < 640) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, []);
 
   return (
     <Navbar className="bg-fuchsia-300/50">
@@ -24,7 +36,9 @@ const Navmenu = () => {
             className="w-12 h-12 sm:w-14 sm:h-14 rounded-full"
           />
         </Link>
-        <p className="font-bold text-xl sm:text-2xl">Chatversation</p>
+        {isMobile ? null : (
+          <p className="font-bold text-xl sm:text-2xl">Chatversation</p>
+        )}
       </NavbarBrand>
 
       <NavbarContent justify="end">
