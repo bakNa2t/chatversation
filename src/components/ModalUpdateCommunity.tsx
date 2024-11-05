ca5091d53354666caef753c04aaa207b0d04fbe4
bakNa2t <ok2.lock@gmail.com>
2024-11-05 10:29:29 +0300

    Added ModalUpdateCommunity to CommunitiesList

---
+++ src/components/ModalUpdateCommunity.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { AppwriteException, ID } from "appwrite";
+import { toast } from "react-toastify";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Input,
+  Spinner,
+} from "@nextui-org/react";
+
+import { appwriteConfig, databases } from "../lib/appwrite/config";
+import { communityStore } from "../lib/zustand/communityStore";
+
+const ModalUpdateCommunity = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [name, setName] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+  const communityState = communityStore();
+
+  const handleCreateChat = () => {
+    setIsLoading(true);
+    databases
+      .createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.communitiesCollectionId,
+        ID.unique(),
+        {
+          name: name,
+          desc: desc,
+        }
+      )
+      .then((res) => {
+        communityState.addCommunity(res);
+        setIsLoading(false);
+        onClose();
+        toast.success("Chat group created successfully", { theme: "colored" });
+      })
+      .catch((error: AppwriteException) => {
+        setIsLoading(false);
+        toast.error(error.message, { theme: "colored" });
+      });
+  };
+
+  return (
+    <>
+      <Button variant="light" radius="full" onPress={onOpen} isIconOnly>
+        <img src="/assets/icons/edit.svg" alt="edit" />
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="center"
+        backdrop="blur"
+      >
+        <ModalContent className="bg-fuchsia-300 shadow-xl shadow-rose-300">
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-[1.5rem]">
+                Update Chat group
+              </ModalHeader>
+              <ModalBody className="gap-6">
+                <Input
+                  label="Name"
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <Input
+                  label="Description"
+                  type="text"
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={onClose}>
+                  Cancel
+                </Button>
+                <Button
+                  color="danger"
+                  onPress={handleCreateChat}
+                  disabled={isLoading}
+                >
+                  {isLoading ? <Spinner color="secondary" /> : "Submit"}
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalUpdateCommunity;
