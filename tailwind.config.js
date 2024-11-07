da485cc1571ec1a1647d17800a878c0d4d523102
bakNa2t <ok2.lock@gmail.com>
2024-11-07 14:04:09 +0300

    Added custom shadow tailwind cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        base: "0 0 10px ##fb7185",
+      },
+      boxShadow: {
+        fuchsia_light: "0 0 3px #f0abfc",
+        fuchsia_medium: "0 0 6px #c026d3",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
