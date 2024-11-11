fcca319ec4663a70dea9959d3971843425f30ff3
bakNa2t <ok2.lock@gmail.com>
2024-11-11 10:15:31 +0300

    Added svg icon to separates file

--- src/_auth/form/SigninForm.tsx
+++ src/_auth/form/SigninForm.tsx
@@ -106,45 +106,23 @@ const SigninForm = () => {
               }
               endContent={
                 <button
-                  className="focus:outline-none"
+                  className="focus:outline-none pb-[calc(0.5rem-1px)]"
                   type="button"
                   onClick={togglePasswordVisibility}
                   aria-label="toggle password visibility"
                 >
                   {isVisible ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="lucide lucide-eye"
-                    >
-                      <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
-                      <circle cx="12" cy="12" r="3" />
-                    </svg>
+                    <img
+                      src="/assets/icons/eye-on.svg"
+                      alt="eye"
+                      className="opacity-50"
+                    />
                   ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="lucide lucide-eye-off"
-                    >
-                      <path d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" />
-                      <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
-                      <path d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" />
-                      <path d="m2 2 20 20" />
-                    </svg>
+                    <img
+                      src="/assets/icons/eye-off.svg"
+                      alt="eye"
+                      className="opacity-50"
+                    />
                   )}
                 </button>
               }
