347621f43cdf13690931268e3c9355f39a50e76d
bakNa2t <ok2.lock@gmail.com>
2024-11-12 11:32:58 +0300

    Updated Modal logout width for mobile

--- src/components/ModalLogout.tsx
+++ src/components/ModalLogout.tsx
@@ -72,6 +72,7 @@ const ModalLogout = ({ isMobile }: { isMobile?: boolean }) => {
         onOpenChange={onOpenChange}
         placement="center"
         backdrop="blur"
+        className={isMobile ? "max-w-[25rem]" : ""}
       >
         <ModalContent className="bg-fuchsia-300 dark:bg-stone-700 shadow-xl shadow-rose-300 dark:shadow-xl dark:shadow-fuchsia-500">
           {(onClose) => (
