0003e0dc9ecb45002a26c600fe255710cdaaab44
bakNa2t <ok2.lock@gmail.com>
2024-11-13 13:54:58 +0300

    Created Message comp

---
+++ src/components/Message.tsx
@@ -0,0 +1,30 @@
+import { Models } from "appwrite";
+
+import { formatTime } from "../lib/utils";
+
+const Message = ({
+  chat,
+  colorClass,
+}: {
+  chat: Models.Document;
+  colorClass?: {
+    box: string;
+    header: string;
+  };
+}) => {
+  return (
+    <div
+      className={`flex flex-col gap-1 max-w-60 sm:max-w-96 rounded-[4rem] sm:rounded-full px-8 py-4 sm:py-2 ${colorClass?.box}`}
+    >
+      <h1 className={`font-bold text-xl border-b-1 ${colorClass?.header}`}>
+        {chat.name}
+      </h1>
+      <p className="break-words">{chat.message}</p>
+      <div className="text-[8px] sm:text-xs text-right text-slate-600/80 dark:text-purple-400/80 italic">
+        {formatTime(chat.$createdAt)}
+      </div>
+    </div>
+  );
+};
+
+export default Message;
