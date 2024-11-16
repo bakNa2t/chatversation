ca3e24711f8b706935b89087638b25b47f826eb2
bakNa2t <ok2.lock@gmail.com>
2024-11-16 09:39:26 +0300

    Added custom-scrollbar cls style

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -3,10 +3,10 @@ import Navmenu from "../components/Navmenu";
 
 const RootLayout = () => {
   return (
-    <div className="h-screen overflow-auto bg-fuchsia-200 dark:bg-stone-700 w-full">
+    <div className="h-screen overflow-auto bg-fuchsia-200 dark:bg-stone-700 w-full custom-scrollbar">
       <Navmenu />
 
-      <section className="p-4 md:py-10 md:px-60">
+      <section className="p-4 md:py-10 md:px-40">
         <Outlet />
       </section>
     </div>
