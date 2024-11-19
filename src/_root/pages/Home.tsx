e2c43d2bf74eb559011f56b4b20a1386165c45c7
bakNa2t <ok2.lock@gmail.com>
2024-11-19 09:39:04 +0300

    Removed idLoading state

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -7,23 +7,18 @@ import ModalCreateCommunity from "../../components/ModalCreateCommunity";
 
 import { account } from "../../lib/appwrite/config";
 import { userStore } from "../../lib/zustand/userStore";
-import { Spinner } from "@nextui-org/react";
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const navigate = useNavigate();
   const isRendered = useRef<boolean>(false);
   const userSessionState = userStore();
 
   useEffect(() => {
     if (!isRendered.current) {
-      setIsLoading(true);
-
       account
         .get()
         .then((res) => {
           userSessionState.updateUser(res);
-          setIsLoading(false);
         })
         .catch(() => {
           userSessionState.resetState();
@@ -31,19 +26,11 @@ const Home = () => {
           toast.error("Your session has expired, please sign in again", {
             theme: "colored",
           });
-          setIsLoading(false);
         });
 
       isRendered.current = true;
     }
   }, []);
-
-  if (isLoading)
-    return (
-      <div className="flex justify-center">
-        <Spinner color="secondary" />
-      </div>
-    );
 
   return (
     <>
