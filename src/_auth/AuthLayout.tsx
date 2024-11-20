4f86a4a6655039b34892bb2bbb6f7f40e4c390a9
bakNa2t <ok2.lock@gmail.com>
2024-11-20 11:06:30 +0300

    Added ThemeBtn to Auth comp

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,13 @@
 import { Outlet } from "react-router-dom";
 
+import ThemeBtn from "../components/ThemeBtn";
+
 const AuthLayout = () => {
   return (
-    <div className="w-full bg-fuchsia-100 dark:bg-stone-800">
+    <div className="relative w-full bg-fuchsia-100 dark:bg-stone-800">
+      <div className="absolute top-2 right-2">
+        <ThemeBtn />
+      </div>
       <Outlet />
     </div>
   );
